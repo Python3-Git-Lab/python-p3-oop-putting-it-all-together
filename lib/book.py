5af2ca40fe526d3f52a89b7b9606e964b91b6fb6
dmmuchoki7 <dmmuchoki7@gmail.com>
2023-12-12 09:16:29 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title= title
+        self.page_count = page_count
     
+    #page getter
+    def get_page(self):
+        return self._page_count
+    
+    #page setter
+    def set_page(self, page_count):
+        if type(page_count)!=int:
+            print("page_count must be an integer")
+        else:
+            self._page_count = page_count
+        
+    #page property
+    page_count = property(get_page,set_page)
+    
+    #turn page
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+     
+    
+#test code
+book = Book("And Then There Were None", 272)
+book.turn_page
+
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,30 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    condition = "New"
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+
+#-------------Size        
+    # size getter
+    def get_size(self):
+        return self._size
+    
+    #size setter
+    def set_size(self,size):
+        if type(size) != int:
+            print("size must be an integer")
+        else:
+            self._size = size
+            print(f"Set size: {size}")
+    
+    #property
+    size = property(get_size,set_size)
+
+#---------condition
+    def cobble(self):
+        print ("Your shoe is as good as new!")
+
+stan_smith = Shoe("Adidas", 9)
+stan_smith.cobble
